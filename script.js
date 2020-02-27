a54fccbd2bb18e5b4c260e59ca1bef4ec8295ef8
elenaandersson <elena.andersson@live.se>
2020-02-27 15:56:48 +0100

    Changed to todo-list, color green, lang eng

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+//Today's date
 
-// This section delete the books from the list when
+let today = new Date();
+let mm = String(today.getMonth() + 1).padStart(2, '0');
+let dd = String(today.getDate()).padStart(2, '0');
+let yyyy = today.getFullYear();
+
+today = dd + '-' + mm + '-' + yyyy;
+document.getElementsByTagName('h2')[0].textContent = today;
+
+
+
+// This section delete the tasks from the list when
 // the user clicks on the delete button
 //Selects the ul
-const list = document.querySelector("#book-list ul");
+const list = document.querySelector("#task-list ul");
 //add evt to ul
 list.addEventListener("click", function(e){
   //check if user clicked a deletebutton
@@ -14,32 +25,32 @@ list.addEventListener("click", function(e){
 });
 
 
-//Adds books
+//Adds tasks
 // selects the form
-const addForm = document.querySelector("#add-book");
+const addForm = document.querySelector("#add-task");
 //Selects "LÄgg till"-button
 const addBtn = document.querySelector(".add-button");
 addBtn.addEventListener("click", function (e){
   e.preventDefault();
 
 
-    //create element for the book
+    //create element for the task
     // get the value from input and store in a const
     const value = addForm.querySelector('input[type="text"]').value;
     if (value.length > 0){
     const li = document.createElement("li");
-    const bookName = document.createElement("span");
+    const taskName = document.createElement("span");
     const deleteBtn = document.createElement("span");
     // add content
-    bookName.textContent = value;
-    deleteBtn.textContent = "Ta bort";
+    taskName.textContent = value;
+    deleteBtn.textContent = 'Delete';
 
     // add class
-    bookName.classList.add("name");
+    taskName.classList.add("name");
     deleteBtn.classList.add("delete");
 
     //append to DOM
-    li.appendChild(bookName);
+    li.appendChild(taskName);
     li.appendChild(deleteBtn);
     list.appendChild(li);
      }
